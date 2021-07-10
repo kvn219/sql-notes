d3d14bfb381df06e93ec0eb24f0741c62843a70b
Kevin Nguyen <kvn219@users.noreply.github.com>
2021-07-10 13:27:31 -0700

    Create subscriptions.sql

---
+++ subscriptions.sql
@@ -0,0 +1,10 @@
+CREATE TABLE subscriptions (
+  user_id INT,
+  start_date DATE,
+  end_date DATE
+);
+
+INSERT INTO subscriptions (user_id, start_date, end_date) VALUES (1, '2019-01-01', '2019-01-31');
+INSERT INTO subscriptions (user_id, start_date, end_date) VALUES (2, '2019-01-15', '2019-01-17');
+INSERT INTO subscriptions (user_id, start_date, end_date) VALUES (3, '2019-01-29', '2019-02-04');
+INSERT INTO subscriptions (user_id, start_date, end_date) VALUES (4, '2019-02-05', '2019-02-10');
